5db6ba53460f545df72e3ad01879619f646e8da9
Thuan Nguyen <cs2be@yahoo.com>
2017-10-18 15:52:10 -0700

    remove comments

--- paddle/scripts/travis/deploy_to_website.sh
+++ paddle/scripts/travis/deploy_to_website.sh
@@ -12,16 +12,9 @@ chmod 400 $TRAVIS_BUILD_DIR/ubuntu.pem
 
 ssh-add $TRAVIS_BUILD_DIR/ubuntu.pem
 
-echo "------------- $TRAVIS_BUILD_DIR"
-ls $TRAVIS_BUILD_DIR
-echo "------------- $TRAVIS_BUILD_DIR/build"
-ls $TRAVIS_BUILD_DIR/build
-echo "------------- $TRAVIS_BUILD_DIR/build/doc"
-ls $TRAVIS_BUILD_DIR/build/doc
-
 mkdir -p $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH
-cp -r $TRAVIS_BUILD_DIR/build/doc/en/html $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH/en
-cp -r $TRAVIS_BUILD_DIR/build/doc/cn/html $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH/cn
+cp -r $TRAVIS_BUILD_DIR/build/doc/en $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH/en
+cp -r $TRAVIS_BUILD_DIR/build/doc/cn $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH/cn
 
 # pull PaddlePaddle.org app and strip
 # https://github.com/PaddlePaddle/PaddlePaddle.org/archive/master.zip
@@ -40,20 +33,13 @@ then
 fi
 mkdir ./stripped_doc
 
-echo "-------------TESTING  rsync -r ..."
-pwd
-ls
-
 python manage.py deploy_documentation $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH $TRAVIS_BRANCH ./stripped_doc documentation
 
 ls $TRAVIS_BUILD_DIR/PaddlePaddle.org-master/portal/stripped_doc/
 
 # debug purpose, show stripped_doc
 # rsync -r ./stripped_doc ubuntu@52.76.173.135:/tmp
-
-cd $TRAVIS_BUILD_DIR
-
-rsync -r PaddlePaddle.org-master/portal/stripped_doc/ ubuntu@52.76.173.135:/var/content/docs
+rsync -r ./stripped_doc/ ubuntu@52.76.173.135:/var/content/docs
 
 chmod 644 $TRAVIS_BUILD_DIR/ubuntu.pem
 rm $TRAVIS_BUILD_DIR/ubuntu.pem
